ba46b0acb5be6ab63c48d9cf8b9f404bc29303d3
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2022-07-12 21:53:59 +0200

    deploy: change connection to mongodb

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -8,34 +8,36 @@ import mongoose from "mongoose";
  */
 const withDatabase = (handler) => async (req, res) => {
   try {
-
     if (mongoose.connections[0].readyState) {
       // Use current db connection
       return handler(req, res);
     }
     // Use new db connection
-    
+
     await mongoose.connect(
-      `mongodb${process.env.IN_ATLAS === "ATLAS" ? "+srv" : ""}://${
-        process.env.DB_USER
-      }:${process.env.DB_PASS}@${process.env.DB_HOST}/${
-        process.env.DB_NAME || "rsr"
-      }?retryWrites=true&w=majority&authSource=admin`,
-      
+      process.env.MONGODB_URI ||
+        `mongodb${process.env.IN_ATLAS === "ATLAS" ? "+srv" : ""}://${
+          process.env.DB_USER
+        }:${process.env.DB_PASS}@${process.env.DB_HOST}/${
+          process.env.DB_NAME || "rsr"
+        }?retryWrites=true&w=majority&authSource=admin`,
+
       {
         useUnifiedTopology: true,
         useNewUrlParser: true,
       }
-      );
-      return handler(req, res);
-    } catch (err) {
-      
-      res.status(500).json({data:null, error:{
+    );
+    return handler(req, res);
+  } catch (err) {
+    res.status(500).json({
+      data: null,
+      error: {
         message: "Error connecting to database",
         code: 500,
         name: "MONGOOSE_CONNECTION_ERROR",
-      }});
-    }
+      },
+    });
+  }
 };
 
 export default withDatabase;
